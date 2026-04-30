cca91428c14578cbd6d5502bf316b52ad513f0a8
Thomas HUYNH <thomashuynh795@gmail.com>
2026-05-01 00:56:10 +0200

    2026-04-30 22:56:10 UTC

--- backend/crates/api/src/person/models/person.rs
+++ backend/crates/api/src/person/models/person.rs
@@ -1,18 +1,25 @@
-use crate::person::person_type_enum::PersonType;
+use crate::{location::domain::models::address::Address, person::person_type_enum::PersonType};
 
 pub struct Person {
     legal_person_type: PersonType,
     first_name: String,
     last_name: String,
+    address: Address,
 }
 
 impl Person {
     #[must_use]
-    pub const fn new(legal_person_type: PersonType, first_name: String, last_name: String) -> Self {
+    pub const fn new(
+        legal_person_type: PersonType,
+        first_name: String,
+        last_name: String,
+        address: Address,
+    ) -> Self {
         Self {
             legal_person_type,
             first_name,
             last_name,
+            address,
         }
     }
 
@@ -35,10 +42,17 @@ impl Person {
     pub const fn get_legal_person_type(&self) -> &PersonType {
         &self.legal_person_type
     }
+
+    #[must_use]
+    pub const fn get_address(&self) -> &Address {
+        &self.address
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::location::domain::{country_enum::Country, thoroughfare_enum::Thoroughfare};
+
     use super::*;
 
     #[test]
@@ -47,6 +61,12 @@ mod tests {
             PersonType::NaturalPerson,
             String::from("John"),
             String::from("Doe"),
+            Address::new(
+                Country::France,
+                Thoroughfare::Street,
+                String::from("1 Street of the Republic"),
+                None,
+            ),
         );
 
         assert!(matches!(
@@ -55,5 +75,13 @@ mod tests {
         ));
         assert!(matches!(person.get_first_name(), "John"));
         assert!(matches!(person.get_last_name(), "Doe"));
+        assert!(matches!(
+            person.get_address().get_country(),
+            Country::France
+        ));
+        assert!(matches!(
+            person.get_address().get_line_1(),
+            "1 Street of the Republic"
+        ));
     }
 }
